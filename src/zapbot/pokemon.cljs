417e4d96b3f025fc24e70b6f9ea92bc43e8a9105
Caio Clavico <caiohclavico@gmail.com>
2026-08-14 19:25:56 -0300

    feat: enhance Pokémon battle mechanics with type advantages and special attack features

--- src/zapbot/pokemon.cljs
+++ src/zapbot/pokemon.cljs
@@ -1,6 +1,7 @@
 (ns zapbot.pokemon
   "Comando !pokemon - batalha entre duas pessoas do chat, cada uma com um
   Pokémon sorteado (nome, imagem e stats via PokeAPI - grátis, sem chave).
+  Ataques consideram vantagem de tipo e têm chance de sair como especial.
   Estado guardado em memória por chat (não sobrevive a reinício do bot)."
   (:require [promesa.core :as p]
             [clojure.string :as str]
@@ -36,13 +37,16 @@
         (p/then (fn [res] (.json res)))
         (p/then (fn [data]
                   (let [dados (js->clj data :keywordize-keys true)]
-                    {:nome   (str/capitalize (:name dados))
-                     :imagem (or (get-in dados [:sprites :other :official-artwork :front_default])
-                                 (get-in dados [:sprites :front_default]))
-                     :hp     (stat-base dados "hp")
-                     :ataque (stat-base dados "attack")
-                     :defesa (stat-base dados "defense")
-                     :veloc  (stat-base dados "speed")}))))))
+                    {:nome    (str/capitalize (:name dados))
+                     :imagem  (or (get-in dados [:sprites :other :official-artwork :front_default])
+                                  (get-in dados [:sprites :front_default]))
+                     :tipos   (mapv #(get-in % [:type :name]) (:types dados))
+                     :hp      (stat-base dados "hp")
+                     :ataque  (stat-base dados "attack")
+                     :defesa  (stat-base dados "defense")
+                     :atq-esp (stat-base dados "special-attack")
+                     :def-esp (stat-base dados "special-defense")
+                     :veloc   (stat-base dados "speed")}))))))
 
 (defn- cabecalho []
   (str "⚡ *Batalha Pokémon do tio " config/bot-name "*\n\n"))
@@ -65,6 +69,60 @@
 (defn- chance-esquiva [pokemon]
   (min 50 (quot (:veloc pokemon) 2)))
 
+(def ^:private tipos-pt
+  {"normal" "Normal" "fire" "Fogo" "water" "Água" "electric" "Elétrico"
+   "grass" "Planta" "ice" "Gelo" "fighting" "Lutador" "poison" "Venenoso"
+   "ground" "Terra" "flying" "Voador" "psychic" "Psíquico" "bug" "Inseto"
+   "rock" "Pedra" "ghost" "Fantasma" "dragon" "Dragão" "dark" "Sombrio"
+   "steel" "Aço" "fairy" "Fada"})
+
+;; multiplicador de dano por [tipo do ataque][tipo do defensor] - só relações
+;; != 1x precisam estar aqui, o resto cai no valor-padrão de 1 (neutro)
+(def ^:private tabela-tipos
+  {"normal"   {"rock" 0.5 "steel" 0.5 "ghost" 0}
+   "fire"     {"grass" 2 "ice" 2 "bug" 2 "steel" 2 "fire" 0.5 "water" 0.5 "rock" 0.5 "dragon" 0.5}
+   "water"    {"fire" 2 "ground" 2 "rock" 2 "water" 0.5 "grass" 0.5 "dragon" 0.5}
+   "electric" {"water" 2 "flying" 2 "electric" 0.5 "grass" 0.5 "dragon" 0.5 "ground" 0}
+   "grass"    {"water" 2 "ground" 2 "rock" 2 "fire" 0.5 "grass" 0.5 "poison" 0.5 "flying" 0.5 "bug" 0.5 "dragon" 0.5 "steel" 0.5}
+   "ice"      {"grass" 2 "ground" 2 "flying" 2 "dragon" 2 "fire" 0.5 "water" 0.5 "ice" 0.5 "steel" 0.5}
+   "fighting" {"normal" 2 "ice" 2 "rock" 2 "dark" 2 "steel" 2 "poison" 0.5 "flying" 0.5 "psychic" 0.5 "bug" 0.5 "fairy" 0.5 "ghost" 0}
+   "poison"   {"grass" 2 "fairy" 2 "poison" 0.5 "ground" 0.5 "rock" 0.5 "ghost" 0.5 "steel" 0}
+   "ground"   {"fire" 2 "electric" 2 "poison" 2 "rock" 2 "steel" 2 "grass" 0.5 "bug" 0.5 "flying" 0}
+   "flying"   {"grass" 2 "fighting" 2 "bug" 2 "electric" 0.5 "rock" 0.5 "steel" 0.5}
+   "psychic"  {"fighting" 2 "poison" 2 "psychic" 0.5 "steel" 0.5 "dark" 0}
+   "bug"      {"grass" 2 "psychic" 2 "dark" 2 "fire" 0.5 "fighting" 0.5 "poison" 0.5 "flying" 0.5 "ghost" 0.5 "steel" 0.5 "fairy" 0.5}
+   "rock"     {"fire" 2 "ice" 2 "flying" 2 "bug" 2 "fighting" 0.5 "ground" 0.5 "steel" 0.5}
+   "ghost"    {"psychic" 2 "ghost" 2 "dark" 0.5 "normal" 0}
+   "dragon"   {"dragon" 2 "steel" 0.5 "fairy" 0}
+   "dark"     {"psychic" 2 "ghost" 2 "fighting" 0.5 "dark" 0.5 "fairy" 0.5}
+   "steel"    {"ice" 2 "rock" 2 "fairy" 2 "fire" 0.5 "water" 0.5 "electric" 0.5 "steel" 0.5}
+   "fairy"    {"fighting" 2 "dragon" 2 "dark" 2 "fire" 0.5 "poison" 0.5 "steel" 0.5}})
+
+(defn- formatar-tipos [tipos]
+  (->> tipos (map #(get tipos-pt % (str/capitalize %))) (str/join "/")))
+
+(defn- multiplicador-vs-tipos [tipo-ataque tipos-defesa]
+  (reduce * (map #(get-in tabela-tipos [tipo-ataque %] 1) tipos-defesa)))
+
+(defn- melhor-multiplicador
+  "Efetividade do ataque: usa o melhor dos tipos de quem ataca (o jogo não
+  tem seleção de golpe) contra todos os tipos de quem defende."
+  [tipos-ataque tipos-defesa]
+  (apply max (map #(multiplicador-vs-tipos % tipos-defesa) tipos-ataque)))
+
+;; chance de qualquer ataque sair como especial (usa atq./def. especial em
+;; vez de ataque/defesa físicos) - decidida pelo jogo, não por quem ataca
+(def ^:private chance-especial 30)
+
+(defn- descricao-ataque [especial?]
+  (if especial? "🔮 *ataque especial*" "💥 *ataque*"))
+
+(defn- texto-efetividade [multiplicador]
+  (cond
+    (> multiplicador 1) "\n🔥 Foi super efetivo!"
+    (< multiplicador 1) "\n😕 Não foi muito efetivo..."
+    :else ""))
+
 (defn- criar-jogo [id nome pokemon]
   {:pokemons {:x pokemon}
    :jogadores {:x id}
@@ -74,9 +132,10 @@
    :vez :x})
 
 (defn- legenda-pokemon [jogador-nome pokemon]
-  (str jogador-nome " entrou com *" (:nome pokemon) "*!\n"
-       "❤️ HP: " (:hp pokemon) " | ⚔️ Ataque: " (:ataque pokemon)
-       " | 🛡️ Defesa: " (:defesa pokemon) " | 💨 Velocidade: " (:veloc pokemon)))
+  (str jogador-nome " entrou com *" (:nome pokemon) "* (" (formatar-tipos (:tipos pokemon)) ")!\n"
+       "❤️ HP: " (:hp pokemon) " | ⚔️ Ataque: " (:ataque pokemon) " | 🛡️ Defesa: " (:defesa pokemon) "\n"
+       "🔮 Atq. Especial: " (:atq-esp pokemon) " | 🌀 Def. Especial: " (:def-esp pokemon)
+       " | 💨 Velocidade: " (:veloc pokemon)))
 
 (defn- enviar-imagem [message url legenda]
   (if url
@@ -138,8 +197,8 @@
           (p/resolved (str (cabecalho) "🚪 Batalha cancelada.")))
       (p/resolved (str (cabecalho) "❓ Não tem nenhuma batalha rolando nesse chat.")))))
 
-(defn- calcular-dano [atacante defensor]
-  (max 1 (+ (- (:ataque atacante) (quot (:defesa defensor) 2)) (rand-int 11))))
+(defn- calcular-dano [poder-ataque poder-defesa]
+  (max 1 (+ (- poder-ataque (quot poder-defesa 2)) (rand-int 11))))
 
 (defn- defender-turno [message]
   (let [cid  (chat-id message)
@@ -188,26 +247,35 @@
              defensor       (get-in jogo [:pokemons alvo-marca])
              defendendo?    (get-in jogo [:defendendo alvo-marca])
              esquivou?      (and defendendo? (< (rand-int 100) (chance-esquiva defensor)))
-             dano-base      (calcular-dano atacante defensor)
-             dano           (cond esquivou? 0
-                                   defendendo? (max 1 (quot dano-base 2))
-                                   :else dano-base)
+             especial?      (< (rand-int 100) chance-especial)
+             poder-ataque   (if especial? (:atq-esp atacante) (:ataque atacante))
+             poder-defesa   (if especial? (:def-esp defensor) (:defesa defensor))
+             multiplicador  (melhor-multiplicador (:tipos atacante) (:tipos defensor))
+             dano-com-tipo  (* multiplicador (calcular-dano poder-ataque poder-defesa))
+             dano           (cond esquivou?             0
+                                   (zero? dano-com-tipo) 0
+                                   defendendo?           (max 1 (js/Math.round (/ dano-com-tipo 2)))
+                                   :else                 (max 1 (js/Math.round dano-com-tipo)))
              hp-novo        (max 0 (- (get-in jogo [:hp alvo-marca]) dano))
              jogo           (-> jogo
                                  (assoc-in [:hp alvo-marca] hp-novo)
                                  (assoc-in [:defendendo alvo-marca] false))
              msg-ataque     (cond
                               esquivou?
-                              (str "💨 *" (:nome defensor) "* esquivou completamente do ataque de *"
-                                   (:nome atacante) "*! Nenhum dano.")
+                              (str "💨 *" (:nome defensor) "* esquivou completamente do " (descricao-ataque especial?)
+                                   " de *" (:nome atacante) "*! Nenhum dano.")
+
+                              (zero? dano)
+                              (str (descricao-ataque especial?) " de *" (:nome atacante) "* não teve efeito em *"
+                                   (:nome defensor) "*! 🚫")
 
                               defendendo?
-                              (str "🛡️ *" (:nome atacante) "* atacou! *" (:nome defensor)
-                                   "* estava se defendendo e sofreu só " dano " de dano.")
+                              (str (descricao-ataque especial?) " de *" (:nome atacante) "*! *" (:nome defensor)
+                                   "* estava se defendendo e sofreu só " dano " de dano." (texto-efetividade multiplicador))
 
                               :else
-                              (str "💥 *" (:nome atacante) "* causou " dano " de dano em *"
-                                   (:nome defensor) "*!"))]
+                              (str (descricao-ataque especial?) " de *" (:nome atacante) "* causou " dano
+                                   " de dano em *" (:nome defensor) "*!" (texto-efetividade multiplicador)))]
          (if (zero? hp-novo)
            (do (swap! jogos dissoc cid)
                (rank/pontuar! cid (get-in jogo [:jogadores atacante-marca])
