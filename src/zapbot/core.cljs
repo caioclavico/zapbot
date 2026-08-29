18f15edf2b71f2a3642258c3c33c40f943927b4b
Caio Clavico <caiohclavico@gmail.com>
2026-08-29 11:00:44 -0300

    pokemon time volta a mostrar no grupo

--- src/zapbot/core.cljs
+++ src/zapbot/core.cljs
@@ -32,13 +32,6 @@
 (defn- chat-id [message]
   (if (.-fromMe message) (.-to message) (.-from message)))
 
-(defn- destinatario-privado [message]
-  ;; Em grupos, `author` identifica quem executou o comando; numa conversa
-  ;; direta, `from` é o próprio contato. Para comandos enviados pelo número
-  ;; conectado, `to` identifica esse número.
-  (or (.-author message)
-      (if (.-fromMe message) (.-to message) (.-from message))))
-
 ;; em desenvolvimento (APP_ENV=development), só processa mensagens do chat de
 ;; teste (DEV_GROUP_ID) - evita responder duplicado nos grupos reais enquanto
 ;; uma instância local roda ao lado da de produção
@@ -54,17 +47,9 @@
         (p/then (fn [resposta]
                   (cond
                     (nil? resposta) nil
-                    (:privado resposta) (let [privado (:privado resposta)
-                                               client  (.-client message)
-                                               destino (destinatario-privado message)]
-                                           (if (string? privado)
-                                             (.sendMessage client destino privado)
-                                             (.sendMessage client destino (:media privado) nil
-                                                           #js {:caption (:texto privado)})))
                     ;; comandos que precisam marcar alguém com @ (ex.: !pokemon,
                     ;; de quem for a vez) resolvem {:texto :mentions} em vez de
-                    ;; uma string simples - todo o resto continua string normal.
-                    ;; `:privado` é enviado sem publicar a resposta no grupo.
+                    ;; uma string simples - todo o resto continua string normal
                     (string? resposta) (.reply message resposta)
                     :else (.reply message (:texto resposta) nil #js {:mentions (clj->js (:mentions resposta))}))))
         (p/catch (fn [err] (js/console.error "Erro ao processar mensagem:" err))))))
